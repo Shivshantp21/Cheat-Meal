b6fda5ba7db1d3b6a314d19163e953277e53cb0e
Ashutosh-IT <kashu19march@gmail.com>
2024-08-10 01:24:48 +0530

    showed food items from backend to frontend

--- frontend/src/components/FoodItem/FoodItem.jsx
+++ frontend/src/components/FoodItem/FoodItem.jsx
@@ -5,12 +5,12 @@ import { StoreContext } from '../../context/StoreContext';
 
 const FoodItem = ({id,name,price,description,image}) => {
 
-  const {CartItem,addToCart,removeFromCart} = useContext(StoreContext);
+  const {CartItem,addToCart,removeFromCart,url} = useContext(StoreContext);
 
   return (
     <div className='food-item'>
         <div className="food-item-img-container">
-            <img className="food-item-img" src={image} alt="" />
+            <img className="food-item-img" src={url+"/images/"+image} alt="" />
             {
                 !CartItem[id] ? <img className="add" onClick={()=>addToCart(id)} src={assets.add_icon_white} alt="" /> 
                 : <div className='food-item-counter'>
--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -1,5 +1,5 @@
 import { createContext, useEffect, useState } from "react";
-import { food_list } from "../assets/assets";
+import axios from "axios"
 
 export const StoreContext = createContext(null);
 
@@ -8,6 +8,8 @@ const StoreContextProvider = (props) => {
     const [CartItem,setCartItem] = useState({});
     const url = "http://localhost:4000";
     const [token,setToken] = useState("");
+
+    const [food_list, setFoodList] = useState([]);
     
     
     const addToCart = (itemId) => {
@@ -34,6 +36,23 @@ const StoreContextProvider = (props) => {
         return totalAmount;
     }
 
+    const fetchFoodList = async()=>{
+        const response = await axios.get(url+"/api/food/list");
+        setFoodList(response.data.data);
+    }
+
+    useEffect(()=>{
+        async function loadData(){
+            await fetchFoodList();
+            if(localStorage.getItem("token")){
+                setToken(localStorage.getItem("token"));
+            }
+        }
+        loadData();
+    },[]);
+    
+
+
     const contextValue = {
         food_list,
         CartItem,
@@ -47,12 +66,7 @@ const StoreContextProvider = (props) => {
     }
 
 
-    useEffect(()=>{
-        if(localStorage.getItem("token")){
-            setToken(localStorage.getItem("token"));
-        }
-    },[]);
-    
+ 
 
     return (
         <StoreContext.Provider value={contextValue}>
